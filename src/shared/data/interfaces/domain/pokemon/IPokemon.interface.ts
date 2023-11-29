7228b79066ba4385819229609fde124cb56bfffc
Jack Miller <jackmiller0899@gmail.com>
2023-11-29 15:50:56 +0700

    Update interfaces

--- src/shared/data/interfaces/domain/pokemon/IPokemon.interface.ts
+++ src/shared/data/interfaces/domain/pokemon/IPokemon.interface.ts
@@ -1,18 +1,26 @@
 import { RecordOf } from 'immutable';
 /**
- * Pokémon are the creatures that inhabit the world of the Pokémon games. 
- * They can be caught using Pokéballs and trained by battling with other 
- * Pokémon. Each Pokémon belongs to a specific species but may take on a 
- * variant which makes it differ from other Pokémon of the same species, 
+ * Pokémon are the creatures that inhabit the world of the Pokémon games.
+ * They can be caught using Pokéballs and trained by battling with other
+ * Pokémon. Each Pokémon belongs to a specific species but may take on a
+ * variant which makes it differ from other Pokémon of the same species,
  * such as base stats, available abilities and typings.
  */
-export interface IPokemon {}
+export interface IPokemon {
+  id: number;
+  name: string;
+  height: number;
+  weight: number;
+  baseExperience: number;
+  order: number;
+  isDefault: boolean;
+}
 
 /**
- * Pokémon are the creatures that inhabit the world of the Pokémon games. 
- * They can be caught using Pokéballs and trained by battling with other 
- * Pokémon. Each Pokémon belongs to a specific species but may take on a 
- * variant which makes it differ from other Pokémon of the same species, 
+ * Pokémon are the creatures that inhabit the world of the Pokémon games.
+ * They can be caught using Pokéballs and trained by battling with other
+ * Pokémon. Each Pokémon belongs to a specific species but may take on a
+ * variant which makes it differ from other Pokémon of the same species,
  * such as base stats, available abilities and typings.
  */
 export type Pokemon = RecordOf<IPokemon>;
--- src/shared/data/services/domain/pokemon/pokemon.service.ts
+++ src/shared/data/services/domain/pokemon/pokemon.service.ts
@@ -1,17 +1,17 @@
+import { ResourceList, ResourceListQuery } from '@interfaces/application/utilities';
 import { IResourceService } from '@interfaces/application/services';
-import { ResourceList } from '@interfaces/application/utilities';
 import { Injectable } from '@angular/core';
 import { Pokemon } from '@interfaces/domain/pokemon';
 
 @Injectable({ providedIn: 'root' })
 export class PokemonService implements IResourceService<Pokemon> {
   constructor() {}
-
+  
   getResource(nameOrId: string): Pokemon {
     throw new Error('Method not implemented.');
   }
-
-  getResourceList(): ResourceList<Pokemon> {
+  
+  getResourceList(query?: ResourceListQuery): ResourceList<Pokemon> {
     throw new Error('Method not implemented.');
   }
 }
