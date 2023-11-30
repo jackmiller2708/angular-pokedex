c6fa2f73aef2bb8d6306aae363700d99b24a6790
Jack Miller <jackmiller0899@gmail.com>
2023-11-30 14:58:53 +0700

    Update Adaptable Record model

--- src/shared/data/models/application/utilities/AdaptableRecord.model.ts
+++ src/shared/data/models/application/utilities/AdaptableRecord.model.ts
@@ -3,9 +3,9 @@ import { Record } from "immutable";
 
 export function AdaptableRecordFactory<R, T extends object>(options: IAdaptableRecordOptions<R, T>): IAdaptableRecordFactory<R, T> {
   const { defaultValues, adaptor, name } = options;
-
-  return Object.assign<Partial<IAdaptableRecordFactory<R, T>>, Record.Factory<T>>(
-    { adaptor, displayName: name }, 
-    Record<T>(defaultValues)
+  
+  return Object.assign<Partial<IAdaptableRecordFactory<R, T>>, any>(
+    Record<T>(defaultValues),
+    { adaptor, displayName: name }
   ) as IAdaptableRecordFactory<R, T>;
 }
