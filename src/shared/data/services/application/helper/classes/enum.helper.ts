bc5f73a84269bd6d0a5fa9f8c1ac78176120fcc2
Jack Miller <jackmiller0899@gmail.com>
2023-12-03 11:48:34 +0700

    Update enum helper

--- src/shared/data/services/application/helper/classes/enum.helper.ts
+++ src/shared/data/services/application/helper/classes/enum.helper.ts
@@ -1,11 +1,19 @@
 type Enum<E> = Record<keyof E, number | string> & { [k: number]: string };
 
 export class EnumHelper {
-  get<E extends Enum<E>>(collection: E, key: keyof E): E[keyof E] {
-    return collection[key];
+  from<E extends Enum<E>>(collection: E) {
+    return new EnumObj(collection);
+  }
+}
+
+class EnumObj<E extends Enum<E>> {
+  constructor(private readonly _enum: E) {}
+
+  get(key: keyof E): E[keyof E] {
+    return this._enum[key];
   }
 
-  has<E extends Enum<E>>(collection: E, key: unknown): key is keyof E {
-    return Object.hasOwn(collection, key as any);
+  has(key: unknown): key is keyof E {
+    return Object.hasOwn(this._enum, key as any);
   }
 }
