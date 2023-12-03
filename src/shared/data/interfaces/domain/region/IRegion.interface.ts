a0f3a4d5cfc45bf68cd92db2bad508cc360b4833
Jack Miller <jackmiller0899@gmail.com>
2023-12-03 12:02:25 +0700

    Update `Region` interface and model

--- src/shared/data/interfaces/domain/region/IRegion.interface.ts
+++ src/shared/data/interfaces/domain/region/IRegion.interface.ts
@@ -1,7 +1,8 @@
+import { IHasAssets } from '@interfaces/application/assets';
 import { Name, NamedResource } from '../utilities';
 import { List, RecordOf } from 'immutable';
 
-export interface IRegion {
+export interface IRegion extends IHasAssets {
   id: number;
   name: string;
   names: List<Name>;
--- src/shared/data/models/domain/region/Region.model.ts
+++ src/shared/data/models/domain/region/Region.model.ts
@@ -2,6 +2,7 @@ import { IRegion, Region as TRegion } from '@interfaces/domain';
 import { AdaptableRecordFactory } from '@models/application/utilities';
 import { IRegion as IRegionDto } from '@interfaces/dtos';
 import { NamedResource } from '../utilities';
+import { AssetInfo } from '@models/application/assets';
 import { List } from 'immutable';
 
 const defaultValues: IRegion = {
@@ -11,11 +12,13 @@ const defaultValues: IRegion = {
   locations: List(),
   pokedexes: List(),
   versionGroups: List(),
+  assetsInfo: List(),
   mainGeneration: NamedResource(),
 };
 
 const adaptor = (values?: IRegionDto): TRegion => {
   const {
+    name,
     names,
     locations,
     version_groups,
@@ -26,9 +29,16 @@ const adaptor = (values?: IRegionDto): TRegion => {
 
   return Region({
     ...others,
+    name,
     locations: List(locations?.map(NamedResource)),
     versionGroups: List(version_groups?.map(NamedResource)),
     pokedexes: List(pokedexes?.map(NamedResource)),
+    assetsInfo: List([
+      AssetInfo({
+        name: `${name}-region-poster-art`,
+        location: `assets/regions/${name}.jpg`,
+      }),
+    ]),
     mainGeneration: NamedResource(main_generation),
   });
 };
