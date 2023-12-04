f4c770c980423dcfc001de969268dad5ac2511a6
Jack Miller <jackmiller0899@gmail.com>
2023-12-04 13:05:02 +0700

    Reset app state service

--- src/app/services/app-state/app-state.service.ts
+++ src/app/services/app-state/app-state.service.ts
@@ -1,26 +1,4 @@
-import { BehaviorSubject, Observable } from 'rxjs';
-import { HelperService } from '@services/application';
 import { Injectable } from '@angular/core';
-import { Region } from '@interfaces/domain';
 
 @Injectable()
-export class AppStateService {
-  private readonly _selectedRegion: BehaviorSubject<Region | undefined>;
-
-  readonly selectedRegion$: Observable<Region>
-
-  get selectedRegion(): Region | undefined {
-    return this._selectedRegion.getValue();
-  }
-
-  constructor(private readonly _helper: HelperService) {
-    const { takeMeaningfulValue } = this._helper.rxjs.operators;
-
-    this._selectedRegion = new BehaviorSubject<Region | undefined>(undefined)
-    this.selectedRegion$ = this._selectedRegion.asObservable().pipe(takeMeaningfulValue());
-  }
-
-  setSelectedRegion(value: Region): void {
-    this._selectedRegion.next(value);
-  }
-}
+export class AppStateService {}
