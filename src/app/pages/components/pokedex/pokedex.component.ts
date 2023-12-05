559962c26c4775ec015f0bba2d3d798f4dc9c453
Jack Miller <jackmiller0899@gmail.com>
2023-12-05 09:28:42 +0700

    Update pokedex component and its resolver

--- src/app/pages/components/pokedex/pokedex.component.ts
+++ src/app/pages/components/pokedex/pokedex.component.ts
@@ -1,13 +1,51 @@
-import { Component } from '@angular/core';
+import { ActivatedRoute, Data } from '@angular/router';
+import { IObserverSafe } from '@interfaces/application';
+import { HelperService } from '@services/application';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
+import { Pokedex } from '@interfaces/domain';
 
 @Component({
   selector: 'app-pokedex',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './pokedex.component.html',
-  styleUrl: './pokedex.component.scss'
+  styleUrl: './pokedex.component.scss',
+  imports: [CommonModule],
+  standalone: true,
 })
-export class PokedexComponent {
+export class PokedexComponent implements IObserverSafe {
+  private readonly _ngDestroy$: Subject<void>;
+  private _dataSource: Pokedex | undefined;
 
+  get dataSource(): Pokedex | undefined {
+    return this._dataSource;
+  }
+
+  constructor(
+    private readonly _activatedRoute: ActivatedRoute,
+    private readonly _helper: HelperService
+  ) {
+    this._ngDestroy$ = new Subject();
+  }
+
+  ngOnInit(): void {
+    this._initData();
+  }
+
+  ngOnDestroy(): void {
+    this._ngDestroy$.next();
+  }
+
+  private _onPokedexData(data: Data): void {
+    this._dataSource = data['dataSource'];
+  }
+
+  private _initData(): void {
+    const { observableRegistrarFactory } = this._helper.rxjs;
+    const { data } = this._activatedRoute;
+
+    const register = observableRegistrarFactory.call(this, this._ngDestroy$);
+
+    register(data, this._onPokedexData);
+  }
 }
--- src/app/pages/components/pokedex/resolver/pokedex.resolver.ts
+++ src/app/pages/components/pokedex/resolver/pokedex.resolver.ts
@@ -1,5 +1,24 @@
-import { ResolveFn } from '@angular/router';
+import { ErrorResponseResolverFn, ResponseError } from '@interfaces/application/errors';
+import { Observable, EMPTY, catchError } from 'rxjs';
+import { ResolveFn, Router } from '@angular/router';
+import { NotFoundError } from '@models/application/errors';
+import { PokedexService } from '@services/domain';
+import { inject } from '@angular/core';
+
+const errorResponseHandlerFactory = (router: Router): ErrorResponseResolverFn<Observable<any>> => {
+  return (error: ResponseError) => {
+    if (error instanceof NotFoundError) {
+      router.navigate(['/']);
+    }
+
+    return EMPTY;
+  };
+};
 
 export const pokedexResolver: ResolveFn<boolean> = (route, state) => {
-  return true;
+  const router = inject(Router);
+
+  return inject(PokedexService)
+    .getResource(route.params['pokedex'])
+    .pipe(catchError(errorResponseHandlerFactory(router)));
 };
