553d68484f519d822868953b0d8ea4bb8a51bc4a
Jack Miller <50993308+jackmiller2708@users.noreply.github.com>
2023-12-09 07:46:28 +0000

    Update app and home component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,27 @@
+import { HeaderComponent, SearchInputComponent } from '@components/atoms';
+import { Component, HostBinding } from '@angular/core';
 import { AppStateService } from './services';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { Component } from '@angular/core';
+
+const imports = [
+  CommonModule,
+  RouterOutlet,
+  HeaderComponent,
+  SearchInputComponent,
+];
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
-  imports: [CommonModule, RouterOutlet],
+  imports,
   providers: [AppStateService],
   standalone: true,
 })
-export class AppComponent {}
+export class AppComponent {
+  @HostBinding('class')
+  private get _classes(): string[] {
+    return ['w-screen', 'h-screen', 'flex', 'flex-col', 'overflow-hidden'];
+  }
+}
