c9461e9645c6c5fbca2d073592fdfe879668bd97
Jack Miller <50993308+jackmiller2708@users.noreply.github.com>
2023-12-09 13:14:49 +0000

    Add resources page

--- src/app/pages/components/index.ts
+++ src/app/pages/components/index.ts
@@ -2,3 +2,4 @@ export * from './home/home.component';
 export * from './region/region.component';
 export * from './pokedex/pokedex.component';
 export * from './pokemon/pokemon.component';
+export * from './resources/resources.component';
