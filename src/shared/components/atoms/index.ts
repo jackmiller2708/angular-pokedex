58419ee87bfdf7030fdf3f3b3845d764e44554e0
Jack Miller <50993308+jackmiller2708@users.noreply.github.com>
2023-12-09 07:45:25 +0000

    Update component index

--- src/shared/components/atoms/index.ts
+++ src/shared/components/atoms/index.ts
@@ -1,3 +1,5 @@
 export * from './badge/badge.component';
 export * from './list/list.component';
 export * from './card/card.component';
+export * from './header/header.component';
+export * from './search-input/search-input.component';
