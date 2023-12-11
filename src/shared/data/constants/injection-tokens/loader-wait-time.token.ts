6361281dbc62b950027596b1425ae02b5d442793
Jack Miller <jackmiller0899@gmail.com>
2023-12-11 10:37:36 +0700

    Update docs

--- src/shared/data/constants/injection-tokens/loader-wait-time.token.ts
+++ src/shared/data/constants/injection-tokens/loader-wait-time.token.ts
@@ -1,3 +1,7 @@
 import { InjectionToken } from "@angular/core";
 
+/**
+ * The injection token that represents the loader wait time,
+ * whose value can be provided by the `AppLoaderWaitTime` provider.
+ */
 export const LOADER_WAIT_TIME = new InjectionToken('The default wait time for screen loader')
--- src/shared/data/constants/providers/LoaderWaitTime.provider.ts
+++ src/shared/data/constants/providers/LoaderWaitTime.provider.ts
@@ -1,6 +1,12 @@
 import { LOADER_WAIT_TIME } from '@constants/injection-tokens';
 import { Provider } from '@angular/core';
 
+/**
+ * Provides the default loader wait time value,
+ * which can be injected using the `LOADER_WAIT_TIME` token.
+ * 
+ * The default value is `300ms`
+ */
 export const AppLoaderWaitTime: Provider = {
   provide: LOADER_WAIT_TIME,
   useValue: 300,
