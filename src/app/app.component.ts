1f7e9d1c09accbf49a4293c1c38a4be5d16ff4cf
Jack Miller <jackmiller0899@gmail.com>
2023-12-11 08:09:35 +0700

    Add RouterEvent handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,12 @@
 import { HeaderComponent, LinkComponent, SearchInputComponent } from '@components/atoms';
+import { Router, RouterOutlet } from '@angular/router';
 import { AppStateService } from './services';
-import { RouterOutlet } from '@angular/router';
+import { IObserverSafe } from '@interfaces/application';
+import { HelperService } from '@services/application';
 import { CommonModule } from '@angular/common';
+import { RouterEvent } from '@models/application/utilities';
 import { Component } from '@angular/core';
+import { Subject } from 'rxjs';
 
 const imports = [
   CommonModule,
@@ -23,4 +27,35 @@ const imports = [
   },
   standalone: true,
 })
-export class AppComponent {}
+export class AppComponent implements IObserverSafe {
+  private readonly _ngDestroy$: Subject<void>;
+
+  constructor(
+    private readonly _appStateService: AppStateService,
+    private readonly _helperService: HelperService,
+    private readonly _router: Router
+  ) {
+    this._ngDestroy$ = new Subject();
+  }
+
+  ngOnInit(): void {
+    this._initData();
+  }
+
+  ngOnDestroy(): void {
+    this._ngDestroy$.next();
+  }
+
+  private _initData(): void {
+    const { observableRegistrarFactory } = this._helperService.rxjs;
+    const { events } = this._router;
+
+    const register = observableRegistrarFactory.call(this, this._ngDestroy$);
+
+    register(events, this._onRouterEvents);
+  }
+
+  private _onRouterEvents(event: RouterEvent) {
+    
+  }
+}
