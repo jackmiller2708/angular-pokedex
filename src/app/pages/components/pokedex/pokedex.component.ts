185c7303621cf0f0abf24e9863e6363a9cebb699
Jack Miller <jackmiller0899@gmail.com>
2023-12-15 13:03:32 +0700

    Update pokedex to use list component

--- src/app/pages/components/pokedex/pokedex.component.ts
+++ src/app/pages/components/pokedex/pokedex.component.ts
@@ -1,9 +1,11 @@
+import { VirtualScrollOption as TVirtualScrollOption } from '@components/atoms/list/interfaces';
 import { BreadcrumbsComponent, PokemonCardComponent } from '@components/molecules';
+import { LinkComponent, ListComponent } from '@components/atoms';
 import { Breadcrumb, IObserverSafe } from '@interfaces/application';
+import { VirtualScrollOptions } from '@components/atoms/list/models';
 import { ActivatedRoute, Data } from '@angular/router';
 import { PokedexPageService } from './services';
 import { HelperService } from '@services/application';
-import { LinkComponent } from '@components/atoms';
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { Pokedex } from '@interfaces/domain';
@@ -15,6 +17,7 @@ const imports = [
   PokemonCardComponent,
   BreadcrumbsComponent,
   LinkComponent,
+  ListComponent
 ];
 
 @Component({
@@ -31,6 +34,7 @@ export class PokedexComponent implements IObserverSafe {
   private _dataSource: Pokedex | undefined;
   private _breadcrumbs: List<Breadcrumb> | undefined;
   private _countDigits: number;
+  private _virtualScrollOptions: TVirtualScrollOption;
 
   get dataSource(): Pokedex | undefined {
     return this._dataSource;
@@ -44,12 +48,20 @@ export class PokedexComponent implements IObserverSafe {
     return this._breadcrumbs;
   }
 
+  get virtualScrollOptions(): TVirtualScrollOption {
+    return this._virtualScrollOptions;
+  }
+
   constructor(
     private readonly _route: ActivatedRoute,
     private readonly _helper: HelperService
   ) {
     this._ngDestroy$ = new Subject();
     this._countDigits = 0;
+    this._virtualScrollOptions = VirtualScrollOptions({
+      itemHeight: 294,
+      itemWidth: 254
+    });
   }
 
   ngOnInit(): void {
